a2e3924abca2ef591b6ffcd31e7fd516e5fa2ce1
weskerp <iagowesley_nc@hotmail.com>
2025-03-04 09:48:09 -0300

    try

--- src/declarations.d.ts
+++ src/declarations.d.ts
@@ -6,4 +6,12 @@ declare module '*.sass' {
     const content: any;
     export default content;
   }
-    
+declare module './componentes/MainContent' {
+    const value: any;
+    export default value;
+}
+
+declare module './componentes/Sidebar' {
+    const value: any;
+    export default value;
+}
